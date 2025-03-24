2c1f975f49dae7e3affddc226dd52659c9445d8a
Haziel MM <hazielmaldonado21@aragon.unam.mx>
2025-03-24 03:07:41 -0600

    Tarea2

---
+++ Tareas/Tarea2/Estructuras de control/app.js
@@ -0,0 +1,16 @@
+let numero1 = parseInt(prompt("Ingresa numero1: "));
+
+let numero2 = parseInt(prompt("Ingresa numero2: "));
+
+if (numero1 > numero2){
+    alert("El numero1 es mayor que el numero2, numero1 es: "+ numero1);
+    console.log("El numero1 es mayor que el numero2, numero1 es: "+ numero1);
+} else if (numero2 > numero1) {
+    alert("El numero2 es mayor que el numero1, el numero2 es: "+ numero2);
+    console.log("El numero2 es mayor que el numero1, , el numero2 es: "+ numero2);
+
+}else {
+    alert("El numero2 y numero1 son iguales, los numeros son: " + numero1);
+    console.log("El numero2 y numero1 son iguales, los numeros son: " + numero1);
+}
+
