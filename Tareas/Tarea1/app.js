168ad81200c1a6ac4c20dae14288e372645b32c0
Haziel MM <hazielmaldonado21@aragon.unam.mx>
2025-03-23 23:41:31 -0600

    Tarea1

---
+++ Tareas/Tarea1/app.js
@@ -0,0 +1,19 @@
+let and0 = false && false;
+let and1 = false && true;
+let and2 = true && false;
+let and3 = true && true;
+
+console.log("false|"+"&&"+"|false| = " +and0);
+console.log("false|"+"&&"+"|true| = "+and1);
+console.log("true|"+"&&"+"|false| = "+and2);
+console.log("true|"+"&&"+"|true| = "+and3);
+
+
+let resultado1 = false || false;
+console.log(resultado1);
+let resultado2 = false || true;
+console.log(resultado2);
+let resultado3 = true || false;
+console.log(resultado3);
+let resultado4 = true || true;
+console.log(resultado4);
