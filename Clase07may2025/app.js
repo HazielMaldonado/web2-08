696b757765efa4fa52cb837334cf446689bb61c7
Haziel <hazielmaldonado21@aragon.unam.mx>
2025-05-09 01:43:02 -0600

    Clase 7Mayo2025

---
+++ Clase07may2025/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const routes = require('./routes/routes');
+
+
+const app = express();
+const port = 3024;
+// Sirve para arhivos estaticos en css, img,etc
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/", routes);
+
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
+});
