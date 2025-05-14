f23dadb8d760ed645a2304c635dbc917f8933fb7
Haziel MM <hazielmaldonado21@aragon.unam.mx>
2025-05-14 14:18:44 -0600

    Avance 14 de Mayo 2025

--- Clase12may2025/app.js
+++ Clase12may2025/app.js
@@ -1,29 +1,79 @@
 const express = require('express');
-const mySql = require('mysql2');
+const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
 
-//configuraion de plantilla
+//menejar peticiones por medio de URl usando POST y por medio de url
+app.use (bodyParser.urlencoded({extended:false}));
 
-app.set('view engine', 'ejs');
+//Configuracion de platillas
+app.set('view engine','ejs');
 
-//conexion a la BD
-const db = mySql.createConnection({
-  host: 'localhost', //host de la base de datos
-  user:'root', //usuario de la base de datos
-  password:'1234', //contraseña de la base de datos
-  database:'node_crud', //Nombre de la base de datos
-  port:3306 //puerto 
-
+//conexion a la DB
+const db=mysql.createConnection({
+    host:'localhost', //server
+    user: 'root', //user
+    password: '1234', //pass
+    database: 'node_crud', //Nombre de la DB
+    port: 3306 //port
 });
 
-//validación de la conexión
-db.connect((err) => {
-  if (err) {
-    console.error('Sin conexion a la DB', err);
-  } else {
-    console.log('Base de datos conectada');
-  }
+//validacion de la conexion a la DB
+db.connect(err => {
+    if(err){
+        console.error('Sin conexion a la DB',err);
+    }else{
+        console.log('Base de datos conectada');
+    }
+});
+
+//Iniciar el servidor
+const port= 3005;
+app.listen(port,()=>{
+    console.log(`http://127.0.0.1:${port}`);
+});
+
+//Mostrar en lista
+app.get('/',(req,res)=>{
+    //Consulta a la DB
+    const consulta = 'SELECT * FROM users';
+    
+    //usamos la DB para conectactarnos a la base de datos
+    db.query(consulta,(err,results)=>{
+        if(err){
+            console.error('No se encontro el usuario',err);
+            res.send("error Comunicate con Soporte");
+        }else{
+            res.render('index',{users: results});
+        }
+    });
+  
+});
+
+//Agregar usuario
+app.post('/add',(req,res)=>{
+    const {name, email}=req.body;
+    const insertarRegistroUsuario ='INSERT INTO users (name,email) VALUES (?,?)';
+    db.query( insertarRegistroUsuario,[name, email],(err)=>{
+        if(err){
+            console.error('No se inserto el registro',err);
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+//Editar usuario
+
+app.get('/edit/:id',(req,res)=>{
+    const {id} =req.params;
+    const editar = 'SELECT *  FROM users WHERE id = ?';
+    db.query(editar,[id],(err,results)=>{
+        if(err){
+            console.error("Error",err);
+        }else{
+            res.render('edit',{user: results[0]});
+        }
+    });
 });
